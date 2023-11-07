4c25a06982d9068dc726c168f2a5998117dc4527
Erin Swan-Siegel <prog.swan@gmail.com>
2023-11-06 19:51:08 -0600

    Update from 11/5

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,5 @@
 """
-Created on Saturday Nocember 4th, 2023
+Created on Saturday November 4th, 2023
 Author: Erin Swan-Siegel
 Direction from @kenjee YouTube
 """
@@ -11,29 +11,30 @@ import pandas as pd
 # data.head()
 # data.replace(to_replace='?')
 
-df = pd.read_csv(r"C:\Users\erins\OneDrive\Desktop\MS_Data_analytics\44-688\Capstone_Project\scraping-glassdoor-selenium\Kaggle_Data.csv", encoding='latin-1')
+df = pd.read_csv(r"C:\Users\erins\OneDrive\Desktop\MS_Data_analytics\44-688\Capstone_Project\scraping-glassdoor-selenium\Company_Rating_Data.csv", encoding='latin-1')
 # Replace Company Name with an ID ?
 
-# Remove "K" from Number of Reviews, Average Company Salary, and Num of Jobs Open
-# Then multiply each number by 1,000
-
 # Parse Number of Employees
+
+df = df.drop(['Number of Reviews','Average Salary','Job Openings','Location Website', 'Company Webpage', 'Global Size 2', 'Company Location', 'Industry 2'], axis=1)
+
+# Check for 'Unknown', -1
+df = df[df['Company Name'] != ['Unknown', '-1']] # none
+df = df[df['Global Size'] != ['Unknown', '-1']] # 883 additional
+df = df[df['Industry'] != ['Unknown', '-1']] # 746 additional
+df = df[df['Description'] != ['Unknown', '-1']] # none
+df = df[df['Company Rating'] != ['Unknown', '-1']] # 144 additional
+df = df[df['Headquarters'] != ['Unknown', '-1']] # 7 additional
+df = df[df['Company Ownership Type'] != ['Unknown', '-1']] # 27 additional
+df = df[df['Year Founded'] != ['Unknown', '-1']] # 1,053 additional
+df = df[df['Est Revenue'] != ['Unknown / Non-Applicable', '-1']] # 1,430 additional
+
+# Remove out of country  --> Import pyap  (.parse)  tkaes in string and extract city, state, zip, etc
+# Data Wrangler for help in cleaning data
+## Split out State or Country into new larger "Geographical" field
+
+# In the paper, mention consolidating Industry into certain categories using python Sequence matcher (diflib import SequenceMatcher) --> for very manually-entered data
+
+# Calculate age
+
 # NLP on Company Description
-
-
-# Remove location; there isn't much there we can really use. 
-# Big companies like Lowe's and Home Depot only have 1 location listed and EY has just "United States". 
-# I don't think it's going to give us much to go on.
-df = df.drop('Location', axis=1)
-
-# Check for -1, NULL, 'Unknown'
-df = df[df['Company rating'] != '-1']
-df = df[df['Number of Reviews'] != '-1']
-df = df[df['Average Company Salary'] != '-1']
-df = df[df['Number of Open Jobs'] != '-1']
-df = df[df['Number of Employees'] != '-1']
-df = df[df['Company rating'] != 'Unknown']
-df = df[df['Number of Reviews'] != 'Unknown']
-df = df[df['Average Company Salary'] != 'Unknown']
-df = df[df['Number of Open Jobs'] != 'Unknown']
-df = df[df['Number of Employees'] != 'Unknown']
