ee4f18638fe45921ab4be161e53552ccdc5d75d8
Erin Swan-Siegel <prog.swan@gmail.com>
2023-11-13 00:30:18 -0600

    Week 4

--- .ipynb_checkpoints/data_cleaning-checkpoint.py
+++ .ipynb_checkpoints/data_cleaning-checkpoint.py
@@ -14,6 +14,7 @@ import pandas as pd
 df = pd.read_csv(r"C:\Users\erins\OneDrive\Desktop\MS_Data_analytics\44-688\Capstone_Project\scraping-glassdoor-selenium\Company_Rating_Data.csv", encoding='latin-1')
 # Replace Company Name with an ID ?
 
+# Parse Number of Employees
 
 df = df.drop(['Number of Reviews','Average Salary','Job Openings','Location Website', 'Company Webpage', 'Global Size 2', 'Company Location', 'Industry 2'], axis=1)
 
@@ -28,10 +29,9 @@ df = df[df['Company Ownership Type'] != ['Unknown', '-1']] # 27 additional
 df = df[df['Year Founded'] != ['Unknown', '-1']] # 1,053 additional
 df = df[df['Est Revenue'] != ['Unknown / Non-Applicable', '-1']] # 1,430 additional
 
-
+# Remove out of country  --> Import pyap  (.parse)  tkaes in string and extract city, state, zip, etc
 # Data Wrangler for help in cleaning data
 ## Split out State or Country into new larger "Geographical" field
-
 
 # In the paper, mention consolidating Industry into certain categories using python Sequence matcher (diflib import SequenceMatcher) --> for very manually-entered data
 
