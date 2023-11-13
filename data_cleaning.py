909d22cfeca54fafa3f9d58745e73b9663e0a890
Erin Swan-Siegel <prog.swan@gmail.com>
2023-11-13 00:34:02 -0600

    Week 5

--- data_cleaning.py
+++ data_cleaning.py
@@ -17,24 +17,17 @@ df = pd.read_csv(r"C:\Users\erins\OneDrive\Desktop\MS_Data_analytics\44-688\Caps
 
 df = df.drop(['Number of Reviews','Average Salary','Job Openings','Location Website', 'Company Webpage', 'Global Size 2', 'Company Location', 'Industry 2'], axis=1)
 
-# Check for 'Unknown', -1
+# Check for 'Unknown', -1; numbers will be reduced due to the removal of Headquarters and Industry
 df = df[df['Company Name'] != ['Unknown', '-1']] # none
 df = df[df['Global Size'] != ['Unknown', '-1']] # 883 additional
-df = df[df['Industry'] != ['Unknown', '-1']] # 746 additional
+df = df[df['Industry'] != ['Unknown', '-1']] # 746 additional ---- REMOVE
 df = df[df['Description'] != ['Unknown', '-1']] # none
 df = df[df['Company Rating'] != ['Unknown', '-1']] # 144 additional
-df = df[df['Headquarters'] != ['Unknown', '-1']] # 7 additional
+df = df[df['Headquarters'] != ['Unknown', '-1']] # 7 additional ----- REMOVE
 df = df[df['Company Ownership Type'] != ['Unknown', '-1']] # 27 additional
 df = df[df['Year Founded'] != ['Unknown', '-1']] # 1,053 additional
 df = df[df['Est Revenue'] != ['Unknown / Non-Applicable', '-1']] # 1,430 additional
 
-
-# Data Wrangler for help in cleaning data
-## Split out State or Country into new larger "Geographical" field
-
-
-# In the paper, mention consolidating Industry into certain categories using python Sequence matcher (diflib import SequenceMatcher) --> for very manually-entered data
-
-# Calculate age
+# All of the non-numeric substitutions need to be laid-out here.
 
 # NLP on Company Description
